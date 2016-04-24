5631bf1b14f258638f26bcaffe1b014dadcd240e
Victor Jolissaint <victor.jolissaint@gmail.com>
2016-04-24 23:40:03 +0100

    Display a link to playlist when created

--- src/clj/save_dw_webapp/routes/home.clj
+++ src/clj/save_dw_webapp/routes/home.clj
@@ -13,9 +13,13 @@
   (fn [req]
     (let [{:keys [access_token]} (oauth/get-authentication-response "hell0" (:params req))
           {:keys [id]} (sptfy/get-current-users-profile {} access_token)
-          playlist-name (utils/create-playlist-name)]
-    (sptfy-playlist-copier/do-copy :user-id id :token access_token :playlist-name-to-copy "Discover Weekly" :playlist-name-new playlist-name :public? true)
-    (layout/render "success.html" {:playlist-name playlist-name}))))
+          playlist-name (utils/create-playlist-name)
+          playlist-id (sptfy-playlist-copier/do-copy :user-id id
+                                                     :token access_token
+                                                     :playlist-name-to-copy "Discover Weekly"
+                                                     :playlist-name-new playlist-name
+                                                     :public? true)]
+    (layout/render "success.html" {:user-id id :playlist-name playlist-name :playlist-id playlist-id}))))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
